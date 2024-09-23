b3bd5d5c1fa590657c32d164837f922954ed9033
yongja <kyj00001@gmail.com>
2024-09-23 19:30:37 +0900

    url settings

--- jeommechu/maps/urls.py
+++ jeommechu/maps/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from .views import RestaurantListView, CafeListView
+from .views import RestaurantListView, CafeListView, restaurant_template_view_function
 
 urlpatterns = [
-    path('restaurants_nearby/', RestaurantListView.as_view(), name='restaurants_nearby'),
-    path('cafes_nearby/', CafeListView.as_view(), name='cafes_nearby'),
+    path('api/restaurants_nearby/', RestaurantListView.as_view(), name='restaurants_nearby'),
+    path('api/cafes_nearby/', CafeListView.as_view(), name='cafes_nearby'),
+    path('restaurants_nearby/', restaurant_template_view_function, name='restaurant_template'),  # HTML 템플릿 렌더링 URL
 ]
